89b62b935e961bbe2bef4b8f513f4b5ba25c712a
dgeemedia <lolumah@byupathway.edu>
2026-04-07 00:35:28 +0100

    layout

--- admin-web/src/components/layout/Layout.tsx
+++ admin-web/src/components/layout/Layout.tsx
@@ -3,7 +3,6 @@ import React, { useEffect } from 'react';
 import { useUIStore } from '@/store/uiStore';
 import Sidebar from './Sidebar';
 import Header from './Header';
-import Footer from '@/layout/Footer';
 
 interface LayoutProps {
   children: React.ReactNode;
@@ -20,19 +19,17 @@ const Layout: React.FC<LayoutProps> = ({ children }) => {
   }, [setIsMobile]);
 
   return (
-    <div className="flex h-screen bg-gray-50 overflow-hidden">
+    <div className="flex h-screen bg-gray-100">
       <Sidebar />
 
-      <div className="flex-1 flex flex-col min-w-0 overflow-hidden">
+      <div className="flex-1 flex flex-col overflow-hidden">
         <Header />
 
-        <main className="flex-1 overflow-y-auto">
-          <div className="max-w-7xl mx-auto px-4 md:px-6 py-6">
+        <main className="flex-1 overflow-y-auto bg-gray-50 p-6">
+          <div className="max-w-7xl mx-auto">
             {children}
           </div>
         </main>
-
-        <Footer />
       </div>
     </div>
   );
